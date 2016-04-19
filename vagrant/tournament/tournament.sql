85e66c55f6750f3229669b93ea4a47c8944cf457
Robert Gardner <gtrgardner@gmail.com>
2016-04-19 15:00:27 -0500

    Added DB schema

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,19 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
 
+CREATE TABLE players(
+	id serial,
+	name text,
+	
+	primary key(id)
+	);
+
+
+CREATE TABLE matches(
+	id serial null references players(id),
+	name text,
+	matches_played int,
+	matches_won int
+);
