b2645a7f62d3ae1fc7bd79500fd56a182ed659fc
robert gardner <gtrgardner@gmail.com>
2016-04-19 18:07:32 -0500

    Add database schema

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,23 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+--DROP DATABASE IF EXISTS tournament;
+--CREATE DATABASE tournament;
 
+\c tournament;
 
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS matches;
+
+CREATE TABLE players(
+	id serial PRIMARY KEY,
+	name text
+);
+
+CREATE TABLE matches(
+	id int REFERENCES players(id),
+	matches_played int,
+	matches_won int,
+	matches_lost int,
+	match_id serial PRIMARY KEY
+);
